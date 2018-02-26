72bc95a69308bded9c845d2cda36adb7aede79ca
Argir Argirov <dj.giro@gmail.com>
2018-02-26 14:49:03 -0500

    poster and styling added

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -6,7 +6,17 @@ class FilmList extends Component {
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
-        {this.props.films.map(film => film.title)}
+        {this.props.films.map(film =>
+          <div className="film-row" key={film.id}>
+            <figure className="film-poster">
+              <img src={'https://image.tmdb.org/t/p/w780/'+film.poster_path} alt="{film.title}" />
+            </figure>
+
+          <div className="film-summary">
+            <h1>{film.title}</h1>
+            <p>{film.release_date}</p>
+          </div>
+        </div> )}
       </div>
     );
   }
