cf990a7dab8233b3acea967bf0c6b193fce9f91c
Argir Argirov <dj.giro@gmail.com>
2018-02-26 17:22:17 -0500

    change className when faves icon clicked

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -7,13 +7,15 @@ class Fave extends Component {
       isFave: false
     }
   }
-  handleClick = (e) => {e.stopPropagation();
+  handleClick = (e) => {
+    e.stopPropagation()
     this.setState({isFave: !this.state.isFave})
     console.log("handling Fave click!")}
 
   render() {
+    var queue = this.state.isFave ? 'remove_from_queue':'add_to_queue'
     return (
-      <div className="film-row-fave add_to_queue">
+      <div className={`film-row-fave ${queue}`}>
         <i className="material-icons" onClick={this.handleClick}>add_to_queue</i>
       </div>
     );
