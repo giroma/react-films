2a14b7f55f052bcde81940d84c8a79543018277c
Argir Argirov <dj.giro@gmail.com>
2018-02-26 14:32:46 -0500

    printng all films from list with .map

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import FilmDetails from './components/FilmDetails';
 import FilmList from './components/FilmList';
+import TMDB from './TMDB';
 
 class App extends Component {
   render() {
     return (
       <div className="film-library">
-        <FilmList/>
+        <FilmList films={TMDB.films}/>
 
-        <FilmDetails/>
+        <FilmDetails films={TMDB.films}/>
       </div>
     );
   }
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -6,6 +6,7 @@ class FilmList extends Component {
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
+        {this.props.films.map(film => film.title)}
       </div>
     );
   }
